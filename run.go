494ccc803b12c094ae59454b544818dfb5fc8940
david birdsong <david.birdsong@gmail.com>
2015-05-11 18:49:00 -0700

    minimal filter

---
+++ run.go
@@ -0,0 +1,27 @@
+package hekaservicetags
+
+import (
+	"code.google.com/p/go-uuid/uuid"
+	"fmt"
+	//"github.com/mozilla-services/heka/client"
+	"github.com/mozilla-services/heka/message"
+	"log"
+	"net/url"
+	"os"
+	"time"
+)
+
+type tags struct {
+	f int64
+}
+
+func (*tags) Run(runner pipeline.FilterRunner, helper pipeline.PluginHelper) error {
+
+	for pack := range runner.InChan() {
+		message.NewStringField(pack.Message, "foo", "bar")
+		pack.Recycle()
+
+	}
+	return nil
+
+}
